aa8455245bd5897ead31e2b0d518856bcbc3bde2
Daniel James <djdany444@gmail.com>
2025-02-26 20:20:50 +0530

    WIP

--- app/controllers/accounts/registrations_controller.rb
+++ app/controllers/accounts/registrations_controller.rb
@@ -9,6 +9,7 @@ class Accounts::RegistrationsController < Devise::RegistrationsController
   private
 
     def check_captcha
+      return
       return if verify_recaptcha
 
       build_resource(sign_up_params)
@@ -21,5 +22,19 @@ class Accounts::RegistrationsController < Devise::RegistrationsController
         flash.now[:recaptcha_error] = flash[:recaptcha_error]
         render :new
       end
+    end
+
+    def after_sign_up_path_for(resource)
+      puts('DJDJDJDJ start')
+      puts(params)
+      puts('DJDJDJDJ end')
+      # if params[:terms_of_service] == '1'
+      #   # Redirect to a specific URL if terms are accepted
+      #   specific_path_for_accepted_terms_path
+      # else
+      #   # Redirect to a different URL if terms are not accepted
+      #   specific_path_for_rejected_terms_path
+      # end
+      default_path
     end
 end
