6c99ee3f46febb468bd4d167f83691fb608c5917
Daniel James <djdany444@gmail.com>
2025-06-16 15:57:05 +0800

    WIP

---
+++ app/webpacker/components/CompetitionResultSubmission/ImportResultsData/index.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import {
+  Accordion,
+  Container, Menu, Message, Segment,
+} from 'semantic-ui-react';
+import UploadResultsJson from '../UploadResultsJson';
+import Errored from '../../Requests/Errored';
+import ImportFromWcaLive from './ImportFromWcaLive';
+import WCAQueryClientProvider from '../../../lib/providers/WCAQueryClientProvider';
+
+const IMPORT_RESULTS_COMPONENTS = {
+  uploadResultsJson: UploadResultsJson,
+  importFromWcaLive: ImportFromWcaLive,
+};
+
+export default function Wrapper({
+  competitionId,
+  alreadyHasSubmittedResult,
+  isWrtViewing,
+  // importFromWcaLiveDisabled,
+}) {
+  return (
+    <WCAQueryClientProvider>
+      <ImportResultsData
+        competitionId={competitionId}
+        alreadyHasSubmittedResult={alreadyHasSubmittedResult}
+        isWrtViewing={isWrtViewing}
+        // importFromWcaLiveDisabled={importFromWcaLiveDisabled}
+      />
+    </WCAQueryClientProvider>
+  );
+}
+
+function ImportResultsData({
+  competitionId,
+  alreadyHasSubmittedResult,
+  isWrtViewing,
+  // importFromWcaLiveDisabled,
+}) {
+  const [activeAccordion, setActiveAccordion] = useState(!alreadyHasSubmittedResult);
+  // const [activeOption, setActiveOption] = useState('uploadResultsJson');
+  // const ActiveComponent = IMPORT_RESULTS_COMPONENTS[activeOption];
+
+  return (
+    <Container fluid>
+      <Accordion fluid styled>
+        <Accordion.Title
+          active={activeAccordion}
+          onClick={() => setActiveAccordion((prevValue) => !prevValue)}
+        >
+          Import Results Data
+        </Accordion.Title>
+        <Accordion.Content active={activeAccordion}>
+          {alreadyHasSubmittedResult
+            ? (
+              <Message warning>
+                Some results are already there, importing results data again will override all
+                of them!
+              </Message>
+            )
+            : (
+              <Message info>
+                Please start by selecting a JSON file to import.
+              </Message>
+            )}
+          <UploadResultsJson
+            competitionId={competitionId}
+            isWrtViewing={isWrtViewing}
+          />
+          {/* <Menu attached="top" tabular>
+            <Menu.Item
+              name="uploadResultsJson"
+              active={activeOption === 'uploadResultsJson'}
+              onClick={() => setActiveOption('uploadResultsJson')}
+            >
+              Upload Result JSON (v0.3)
+            </Menu.Item>
+            <Menu.Item
+              name="importFromWcaLive"
+              active={activeOption === 'importFromWcaLive'}
+              onClick={() => setActiveOption('importFromWcaLive')}
+              disabled={importFromWcaLiveDisabled}
+            >
+              Import from WCA Live
+            </Menu.Item>
+          </Menu>
+          <Segment attached="bottom">
+            {ActiveComponent
+              ? (
+                <ActiveComponent
+                  competitionId={competitionId}
+                />
+              )
+              : <Errored error={`No component found for active option: ${activeOption}`} />}
+          </Segment> */}
+        </Accordion.Content>
+      </Accordion>
+    </Container>
+  );
+}
