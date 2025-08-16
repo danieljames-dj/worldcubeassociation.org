0e050fe0fd9a3609319d419e326d487426aecb8b
Daniel James <djdany444@gmail.com>
2025-08-16 17:24:02 +0530

    WIP

--- app/models/tickets_edit_person.rb
+++ app/models/tickets_edit_person.rb
@@ -8,6 +8,11 @@ class TicketsEditPerson < ApplicationRecord
     closed: "closed",
   }
 
+  enum :change_type, {
+    fix: "fix",
+    update: "update",
+  }
+
   has_one :ticket, as: :metadata
   has_many :tickets_edit_person_fields
 
--- db/schema.rb
+++ db/schema.rb
@@ -1378,6 +1378,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_26_030259) do
     t.string "wca_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "change_type"
   end
 
   create_table "tickets_edit_person_fields", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
